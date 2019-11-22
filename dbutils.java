751ea4fffd61fdef793c66f059ba40542315980f
mounika <lakshmimouni12345@gmail.com>
2019-11-22 00:12:52 -0800

    update dbutils.java

--- dbutils.java
+++ dbutils.java
@@ -1,9 +1,12 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 hai
 hello
 using revert command here
 updated contents in that fie will going to delete
+=======
+>>>>>>> parent of df7068e... update db file
 =======
 >>>>>>> parent of df7068e... update db file
 =======
