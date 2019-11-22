10c8c76fea79afb0117e5cdb9c6d3d9c110e1789
mounika <lakshmimouni12345@gmail.com>
2019-11-22 00:25:22 -0800

    Revert "update"
    This reverts commit 781cd461d9717947bdf29ca1a4691011e738bd93.

--- dbutils.java
+++ dbutils.java
@@ -2,13 +2,10 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 hai
 hello
 using revert command here
 updated contents in that fie will going to delete
-=======
->>>>>>> parent of df7068e... update db file
 =======
 >>>>>>> parent of df7068e... update db file
 =======
